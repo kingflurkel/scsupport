c9d634a3d2229f970a17f45e6487e9cd8a7c746a
Michael Thuy <michael@thuy.be>
2018-07-27 04:55:42 +0200

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const chalk = require('chalk');
 const app = require('express')();
 const http = require('http');
+const Web3 = require('web3');
+const ipfs = require('nano-ipfs-store').at('https://ipfs.infura.io:5001');
+var hashtagListAbi = require('./hashtagList.json');
+var web3;
+var hashtagListInstance;
 const level = require('level');
+
 
 // Hi there, this is Swarm City Support. 
 // If you are human, press 1. 
@@ -10,6 +16,48 @@ const level = require('level');
 // I can fetch stuff from Ethereum for you. 
 // Tell me what you want.
 // We'll be serving it up on our websocket, fi: 'hashtagList', string hastagListAddress
+
+const web3WsProvider = new Web3.providers.WebsocketProvider('wss://kovan.infura.io/ws');
+
+web3WsProvider.on('error', () => {
+    console.log('WebSocketProvider Error');
+}); 
+
+web3WsProvider.on('end', () => {
+    console.log('WebSocketProvider Disconnect');
+}); 
+
+web3WsProvider.on('connect', () => {
+    console.log(chalk.blue('WebSocketProvider Connected!'));
+    web3 = new Web3(web3WsProvider);
+    hashtagListInstance = new web3.eth.Contract(hashtagListAbi, '0x5148944fc8cc745a53258a61509f33165026a9b8');
+    hashtagListInstance.events.allEvents({ fromBlock: 8149480 }, function(e,r) {
+        console.log(chalk.yellow(r.event), '\n', chalk.gray(r.transactionHash));
+        processHashtagEvent(r.returnValues, r.transactionHash);
+    });
+
+    newBlockHeadersSubscription = web3.eth.subscribe('newBlockHeaders', (error, result) => {
+        if (result) {
+            blockUpdate(result.number);
+        }
+    });
+}); 
+
+async function blockUpdate (number) {
+    console.log('Block: ', number);
+    // also tell some website online i'm still alive
+};
+
+async function processHashtagEvent (r, tx) {
+    var db = await level('./db');
+    var metaData = await ipfs.cat(r.hashtagMetaIPFS);
+    shelf = await db.get('hashtagListStore');
+    var store = JSON.parse(shelf);
+    store[tx] = JSON.parse(metaData);
+    shelf = JSON.stringify(store);
+    await db.put('hashtagListStore', shelf);
+    db.close();
+};
 
 const server = require('http').createServer(app);
 var port = process.env.PORT || 8080;
@@ -23,15 +71,15 @@ const io = require('socket.io')(server, {
     transports: ['websocket', 'xhr-polling'],
 });
 
-app.get('/hastagList', async function (req, res) {
+app.get('/', async function (req, res) {
     console.log("getHashtagList");
-    res.send(await getHashtagList());
+    res.send(getHashtagList());
 });
 
-app.get('/', async function (req, res) {
-    console.log("hi!");
-    res.send("Hi there!");
-});
+// app.get('/', async function (req, res) {
+//     console.log("hi!");
+//     res.send("Hi there!");
+// });
 
 io.on('connection', function (socket) {
     console.log(socket);
@@ -41,7 +89,6 @@ io.on('connection', function (socket) {
 });
 
 async function getHashtagList () {
-    var db = level('./db');
     var shelf = await db.get('hashtagListStore');
     db.close();
     var store = JSON.parse(shelf);
@@ -49,7 +96,6 @@ async function getHashtagList () {
 };
 
 async function getHashtagItems (address) {
-    var db = level('./db');
     var shelf = await db.get('hashtag-'+address);
     db.close();
     var store = JSON.parse(shelf);
